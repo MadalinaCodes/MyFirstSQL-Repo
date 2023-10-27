c4608be86e693919c59951050f75b3f1a2a2aea3
MadalinaCodes <andon.madalina@yahoo.com>
2023-10-27 15:13:56 -0700

    first commit

---
+++ MyFirstQuery.sql
@@ -0,0 +1,7 @@
+show databases;
+
+select * from information_schema.tables;
+
+select * from information_schema.views;
+
+/* SQL este un limbaj de programare, pot sa lucrez cu el prin executarea unor comenzi de la tastatura, sistemul le preia ca o carte de telefon, in baza de date pot sa stochez diverse informatii*/
